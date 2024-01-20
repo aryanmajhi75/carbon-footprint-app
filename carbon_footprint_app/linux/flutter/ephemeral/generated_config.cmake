b42f30199d266bfac2fbd0b22fdc793e9a355667
aryan <70305149+aryanmajhi75@users.noreply.github.com>
2024-01-20 10:46:11 +0530

    init

---
+++ carbon_footprint_app/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/saruo/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/saruo/Documents/GitHub/carbon-footprint-app/carbon_footprint_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/saruo/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/saruo/Documents/GitHub/carbon-footprint-app/carbon_footprint_app"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZjdhOWQwODAwZjJhNWRhMTY2ZGJlMGViOWZiMjQ3NjAxODI2OWIxLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/saruo/Documents/GitHub/carbon-footprint-app/carbon_footprint_app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/saruo/Documents/GitHub/carbon-footprint-app/carbon_footprint_app/lib/main.dart"
+)
