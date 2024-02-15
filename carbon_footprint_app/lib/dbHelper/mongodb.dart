7249ecc08924efea826d88f67fc5151999bf0c0f
aryan <majhiaryan13@gmail.com>
2024-02-15 11:46:00 +0530

    gitignore update

--- carbon_footprint_app/lib/dbHelper/mongodb.dart
+++ carbon_footprint_app/lib/dbHelper/mongodb.dart
@@ -1,1 +1,40 @@
-import "package:flutter/material.dart";
+import 'package:mongo_dart/mongo_dart.dart';
+
+const dbName = 'mongo-dart-example';
+const dbAddress = '127.0.0.1';
+
+const defaultUri = 'mongodb://$dbAddress:27017/$dbName';
+
+void main() async {
+  var db = Db(defaultUri);
+  await db.open();
+
+  Future cleanupDatabase() async {
+    await db.close();
+  }
+
+  if (!db.masterConnection.serverCapabilities.supportsOpMsg) {
+    return;
+  }
+
+  var collectionName = 'insert-one';
+  await db.dropCollection(collectionName);
+  var collection = db.collection(collectionName);
+
+  var ret = await collection.insertOne(<String, dynamic>{
+    'name': 'Tom',
+    'age': 23,
+    'num_of_members': 3,
+  });
+
+  if (!ret.isSuccess) {
+    print('Error detected in record insertion');
+  }
+
+  var res = await collection.findOne();
+
+  print('Fetched ${res?['name']}');
+  // Tom
+
+  await cleanupDatabase();
+}
