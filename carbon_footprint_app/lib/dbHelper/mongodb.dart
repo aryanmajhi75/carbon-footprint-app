ac4bbb92fe957bbb1bad4f851c50f8d2699c8da7
aryan <majhiaryan13@gmail.com>
2024-02-18 20:17:52 +0530

    mongo db init

--- carbon_footprint_app/lib/dbHelper/mongodb.dart
+++ carbon_footprint_app/lib/dbHelper/mongodb.dart
@@ -1,40 +1,53 @@
 import 'package:mongo_dart/mongo_dart.dart';
 
-const dbName = 'mongo-dart-example';
-const dbAddress = '127.0.0.1';
+const dbName = 'Ecofy';
 
-const defaultUri = 'mongodb://$dbAddress:27017/$dbName';
+const defaultUri =
+    'mongodb+srv://majhiaryan13:TPK4jb57idXbdRsJ@cluster0.bkeqf8e.mongodb.net/?retryWrites=true&w=majority';
 
-void main() async {
+void initDb() async {
   var db = Db(defaultUri);
   await db.open();
-
-  Future cleanupDatabase() async {
-    await db.close();
-  }
-
-  if (!db.masterConnection.serverCapabilities.supportsOpMsg) {
-    return;
-  }
-
-  var collectionName = 'insert-one';
-  await db.dropCollection(collectionName);
-  var collection = db.collection(collectionName);
-
-  var ret = await collection.insertOne(<String, dynamic>{
-    'name': 'Tom',
-    'age': 23,
-    'num_of_members': 3,
-  });
-
-  if (!ret.isSuccess) {
-    print('Error detected in record insertion');
-  }
-
-  var res = await collection.findOne();
-
-  print('Fetched ${res?['name']}');
-  // Tom
-
-  await cleanupDatabase();
 }
+
+void find() async {}
+
+void findAll() async {}
+
+void insertData() async {}
+
+void updateData() async {}
+
+void deleteData() async {}
+
+// void main() async {
+
+//   Future cleanupDatabase() async {
+//     await db.close();
+//   }
+
+//   if (!db.masterConnection.serverCapabilities.supportsOpMsg) {
+//     return;
+//   }
+
+//   var collectionName = 'testCollection';
+//   await db.dropCollection(collectionName);
+//   var collection = db.collection(collectionName);
+
+//   var ret = await collection.insertOne(<String, dynamic>{
+//     'name': 'Tom',
+//     'age': 23,
+//     'num_of_members': 3,
+//   });
+
+//   if (!ret.isSuccess) {
+//     print('Error detected in record insertion');
+//   }
+
+//   var res = await collection.findOne();
+
+//   print('Fetched ${res?['name']}');
+//   // Tom
+
+//   await cleanupDatabase();
+// }
